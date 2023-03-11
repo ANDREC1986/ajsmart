e2568e1eaf057778e10347544c25ab1b0e17f840
André Carvalhal <andrecar2021@hotmail.com>
2023-03-10 23:17:10 -0300

    csschange

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ echo '<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="formatos.css?version=1">
+    <link rel="stylesheet" href="formatos.css?version=2">
     <title>AJ Smart 2023</title>
 </head>
 <body>
